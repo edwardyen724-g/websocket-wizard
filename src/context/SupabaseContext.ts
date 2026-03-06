73fb7122012a36d2905ddef3f3630d432359c53a
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 04:19:10 -0800

    feat: add missing module src/context/SupabaseContext.ts

---
+++ src/context/SupabaseContext.ts
@@ -0,0 +1,9 @@
+import React, { createContext, useContext } from 'react';
+
+const SupabaseContext = createContext(null);
+
+export const SupabaseProvider = ({ children }) => {
+  return <SupabaseContext.Provider value={{}}>{children}</SupabaseContext.Provider>;
+};
+
+export const useSupabase = () => useContext(SupabaseContext);
