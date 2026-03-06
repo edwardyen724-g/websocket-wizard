676cefc6b487d8adafe33d9921309fa937bb717a
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 04:19:13 -0800

    feat: add missing module src/components/templates/index.tsx

---
+++ src/components/templates/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const WebSocketIntegrationGuide = () => <div>WebSocketIntegrationGuide</div>;
+export const TanStackTemplates = () => <div>TanStackTemplates</div>;
+export const LiveCodeSnippets = () => <div>LiveCodeSnippets</div>;
+export const ErrorHandlingGuide = () => <div>ErrorHandlingGuide</div>;
+export const CommunityForum = () => <div>CommunityForum</div>;
